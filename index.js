2e5be40c7ee7d201c09e31b9f3b424da1a614a24
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-08 20:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ function theBeatlesPlay(){
 
 //johnLennonFacts 
 
+/*
 var facts = [  "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice" ];
-
-function johnLennonFacts(){
+*/
+function johnLennonFacts(facts){
   var factsArray = [];
   var fL = facts.length;
   var i = 0;
